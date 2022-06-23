b46dbb45de75ee4e901fd84b9752893ec025b1f3
Capassoft <capassomiguel@gmail.com>
2022-06-23 22:20:12 +0200

    Con formik: creado registerForm y customFields(campos reutilizables)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.scss";
 import TaskList from "./components/container/tasks";
+import RegisterForm from "./components/pure/forms/registerForm";
 
 function App() {
     return (
         <div className="App">
             <main className="App-main">
-                <TaskList />
+                {/* <TaskList /> */}
+                <RegisterForm />
             </main>
         </div>
     );
--- src/components/pure/forms/registerForm.jsx
+++ src/components/pure/forms/registerForm.jsx
@@ -1,14 +1,69 @@
-import { useState } from "react";
+import React from "react";
+import { Formik, Form } from "formik";
+import * as yup from "yup";
+import { MyTextInput } from "./customFields";
+
+let validationSchema = yup.object().shape({
+    firstName: yup.string().required("Required").max(12),
+    lastName: yup.string().required("Required").max(12),
+    email: yup.string().email().required("Required"),
+    password: yup.string().required("Required").min(6).max(20),
+});
 
 const RegisterForm = () => {
-    const initialData = {
-        user: "",
-        name: "",
-        email: "",
-        password: "",
-    };
-    const [data, setData] = useState(initialData);
-    return <></>;
+    // Pass the useFormik() hook initial form values and a submit function that will
+    // be called when the form is submitted
+
+    return (
+        <>
+            <h1>Register</h1>
+            <Formik
+                initialValues={{
+                    firstName: "",
+                    lastName: "",
+                    email: "",
+                    password: "",
+                }}
+                validationSchema={validationSchema}
+                onSubmit={(values, { setSubmitting }) => {
+                    setTimeout(() => {
+                        alert(JSON.stringify(values, null, 2));
+                        setSubmitting(false);
+                    }, 400);
+                }}
+            >
+                <Form className="form">
+                    <MyTextInput
+                        label="First Name"
+                        name="firstName"
+                        type="text"
+                        placeholder="First name"
+                    />
+                    <MyTextInput
+                        label="Last Name"
+                        name="lastName"
+                        type="text"
+                        placeholder="Last name"
+                    />
+                    <MyTextInput
+                        label="Email"
+                        name="email"
+                        type="email"
+                        placeholder="Email"
+                    />
+                    <MyTextInput
+                        label="Password"
+                        name="password"
+                        type="password"
+                        placeholder="Password"
+                    />
+                    <button className="btn btn-outline-dark" type="submit">
+                        Submit
+                    </button>
+                </Form>
+            </Formik>
+        </>
+    );
 };
 
 export default RegisterForm;
